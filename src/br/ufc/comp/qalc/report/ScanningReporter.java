73e3d191fa5b2908dd6e68abb0f6a11581f6c629
Arthur Araruna <ararunaufc@gmail.com>
2019-02-26 16:32:11 -0300

    Nova classe para relatar resultados da Análise Léxica.
    - Nesta classe, apenas os identificadores dos tokens são reportados.

--- src/br/ufc/comp/qalc/report/ScanningReporter.java
+++
@@ -1,4 +0,0 @@
-package br.ufc.comp.qalc.report;
-
-public class ScanningReporter implements MessageConsumer {
-}
---
+++ src/br/ufc/comp/qalc/report/TokensReporter.java
@@ -0,0 +1,26 @@
+package br.ufc.comp.qalc.report;
+
+import br.ufc.comp.qalc.report.messages.BasicReporter;
+import br.ufc.comp.qalc.report.messages.Message;
+import br.ufc.comp.qalc.report.messages.NewTokenMessage;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TokensReporter extends BasicReporter {
+
+    public TokensReporter(OutputStream stream) {
+        super(stream);
+    }
+
+    @Override
+    public void consume(Message message) {
+        if(message instanceof NewTokenMessage) {
+            try {
+                output.write(String.format("(%s)\n", ((NewTokenMessage)message).getToken().getTokenIdentifier()));
+            } catch (IOException e) {
+                reportFailure(e);
+            }
+        }
+    }
+}
