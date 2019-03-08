cee5d075814006805d271d614b6d310a14e48e5b
Arthur Araruna <ararunaufc@gmail.com>
2019-03-08 17:18:45 -0300

    Documentação do `ResourcesManager`.

--- src/main/java/br/ufc/comp/qalc/ResourcesManager.java
+++ src/main/java/br/ufc/comp/qalc/ResourcesManager.java
@@ -5,17 +5,43 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Gerencia os recursos do programa que devem ser liberados de forma automatizada
+ * ao final da execução.
+ * <p>
+ * O uso mais útil no momento é o de garantir que a saída dos relatores seja exibida
+ * antes que o programa encerre.
+ */
 public class ResourcesManager {
+    /**
+     * Todos os objetos que implementem a interface {@link Closeable} que devem ser gerenciados.
+     */
     private static Set<Closeable> managedCloseables = new HashSet<>();
 
+    /**
+     * Adicionar o recurso ao gerenciador na intenção de ser liberado de forma automatizada.
+     * @param resource recurso a ser gerenciado.
+     */
     public static void manage(Closeable resource) {
         managedCloseables.add(resource);
     }
 
+    /**
+     * Desistir do gerenciamento do recurso informado.
+     *
+     * @param resource recurso a remover do gerenciamento.
+     * @return {@code true} caso removido, {@code false} caso não estava sendo gerenciado.
+     */
     public static boolean yield(Closeable resource) {
         return managedCloseables.remove(resource);
     }
 
+    /**
+     * Libera todos os recursos sob a gestão deste gerenciador.
+     *
+     * @param printStackTrace Se as exceções devem ser impressas.
+     * @return a quantidade de recursos cuja liberação ocasionou uma exceção.
+     */
     public static long shutdown(boolean printStackTrace) {
         long failed = 0;
 
